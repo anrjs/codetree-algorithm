cf6c2987bd2c60c9f4e769f1344b486b33a75a33
anrjs <160237843+anrjs@users.noreply.github.com>
2026-01-23 15:04:06 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260123/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a,b,c;
+    cin>>a>>b>>c;
+    cout<<fixed;
+    cout.precision(3);
+    cout<<a<<'\n';
+    cout<<b<<'\n';
+    cout<<c;
+    return 0;
+}

075a8083c1a87553d526eb5d84b38bb7e018cb93
anrjs <160237843+anrjs@users.noreply.github.com>
2026-01-23 14:52:31 +0900

    Title: 달에서 무게 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260123/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int weight = 13;
+    double gravity_in_moon = 0.165;
+    cout<<fix;
+    cout.precision(6);
+    cout<<weight*gravity_in_moon;
+    return 0;
+}

a5743a04dc0e6d7367f37aff8bfd5c0b87ff8d66
anrjs <160237843+anrjs@users.noreply.github.com>
2026-01-27 20:17:13 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260127/문자열 출력하기/print-string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N,i;
+    cin>>N;
+    for(i=0;i<N;i++) cout<<"LeebrosCode"<<endl;
+    return 0;
+}

be3af9d3fbc9d79f7f12f6f38aea8eadcf2ba28a
anrjs <160237843+anrjs@users.noreply.github.com>
2026-01-27 20:15:44 +0900

    Title: 문자 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 260127/문자 출력하기/print-text.cpp
+++ 260127/문자 출력하기/print-text.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    for(int i=0;i<8;i++) cout<<'G';
+    char c;
+    cin>>c;
+    for(int i=0;i<8;i++) cout<<c;
     return 0;
 }

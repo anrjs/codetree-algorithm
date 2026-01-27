33d9e94fcb28c8128dd06fc6f78e9dc99d6079a0
anrjs <160237843+anrjs@users.noreply.github.com>
2026-01-27 20:20:08 +0900

    Title: N번 더하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260127/N번 더하기/add-n-times.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A,N,i;
+    cin>>A>>N;
+    for(i=1;i<=N;i++){
+        cout<<A+N*i<<endl;
+    }
+    return 0;
+}

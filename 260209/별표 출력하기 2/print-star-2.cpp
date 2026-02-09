6a2bfe8d3bf7950fa3e9d39653c53027283be396
anrjs <160237843+anrjs@users.noreply.github.com>
2026-02-09 19:14:40 +0900

    Title: 별표 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260209/별표 출력하기 2/print-star-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin>>N;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N-i;j++)
+        cout<<"* ";
+        cout<<endl;
+    }
+    return 0;
+}
